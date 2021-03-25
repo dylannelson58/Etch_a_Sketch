e0d23d59322e68eeaffdad0139d584ba0706dc77
dylan <dylannelson58@gmail.com>
2021-03-25 08:34:41 -0600

    Add project files.

---
+++ Etch_a_Sketch.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Etch_a_Sketch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.DrawWaveformsButton = New System.Windows.Forms.Button()
+        Me.SelectColorButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DisplayPictureBox
+        '
+        Me.DisplayPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DisplayPictureBox.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(12, 12)
+        Me.DisplayPictureBox.Name = "DisplayPictureBox"
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(776, 346)
+        Me.DisplayPictureBox.TabIndex = 0
+        Me.DisplayPictureBox.TabStop = False
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(396, 366)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(110, 61)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'DrawWaveformsButton
+        '
+        Me.DrawWaveformsButton.Location = New System.Drawing.Point(233, 366)
+        Me.DrawWaveformsButton.Name = "DrawWaveformsButton"
+        Me.DrawWaveformsButton.Size = New System.Drawing.Size(110, 62)
+        Me.DrawWaveformsButton.TabIndex = 1
+        Me.DrawWaveformsButton.Text = "Draw Waveforms"
+        Me.DrawWaveformsButton.UseVisualStyleBackColor = True
+        '
+        'SelectColorButton
+        '
+        Me.SelectColorButton.Location = New System.Drawing.Point(72, 366)
+        Me.SelectColorButton.Name = "SelectColorButton"
+        Me.SelectColorButton.Size = New System.Drawing.Size(110, 61)
+        Me.SelectColorButton.TabIndex = 1
+        Me.SelectColorButton.Text = "Select Color"
+        Me.SelectColorButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(557, 366)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(110, 61)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Etch_a_Sketch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SelectColorButton)
+        Me.Controls.Add(Me.DrawWaveformsButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DisplayPictureBox)
+        Me.Name = "Etch_a_Sketch"
+        Me.Text = "Form1"
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DisplayPictureBox As PictureBox
+    Friend WithEvents ColorDialog As ColorDialog
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents DrawWaveformsButton As Button
+    Friend WithEvents SelectColorButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+End Class
---
+++ Etch_a_Sketch.vb
@@ -0,0 +1,192 @@
+﻿Option Explicit On
+Option Strict On
+
+'Dylan Nelson
+'RCET0265 
+'Spring 2021
+'Etch A Sketch
+'https://github.com/dylannelson58/Etch_a_Sketch
+
+Imports System.Math
+Imports System.Threading.Thread
+
+Public Class Etch_a_Sketch
+    Dim g As Graphics
+    Dim mainPen As New Pen(Color.Black)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click
+
+
+        ColorDialog.ShowDialog()
+        Me.mainPen.Color = ColorDialog.Color
+        'PenColor(ColorDialog.Color)
+    End Sub
+    Sub UpdateParms()
+        'g.PageUnit = GraphicsUnit.Pixel
+        'g.PixelOffsetMode = Drawing2D.PixelOffsetMode.HighSpeed
+
+        'Scale 
+        'This will be the width of the scaled graphics object 
+        Dim xMax As Single = 360
+        Dim xScale As Single = CSng(DisplayPictureBox.Width / xMax)
+
+        'This will be the height of the scaled graphics object 
+        Dim yMax As Single = 100
+        'half the height in pixels is to 100 units
+        '100 up, 100 down
+        Dim yScale As Single = CSng(((DisplayPictureBox.Height) / 2) / yMax)
+        Me.g.ScaleTransform(xScale, yScale) 'apply scale
+
+        'Offset 
+        'y=0 to vertical center
+        Dim yOffset As Single = CSng(yMax)
+        'left edge
+        Dim xOffset As Single
+        Me.g.TranslateTransform(xOffset, yOffset)
+
+        'Rotation 0
+        Dim rotation As Single
+        Me.g.RotateTransform(rotation)
+    End Sub
+
+    'Function PenColor(newColor As Color) As Color
+    '    Static _penColor As Color
+    '    Return _penColor
+    'End Function
+    Sub MouseDraw(x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer) ', mainpen.Color As Color)
+        'constructor for the graphics object
+        Dim g As Graphics = DisplayPictureBox.CreateGraphics
+        'Dim penColor As Color
+        'penColor = Color.Red
+        Dim pen As New Pen(Me.mainPen.Color)
+        'draws the line
+        g.DrawLine(pen, x1, y1, x2, y2)
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+    Private Sub DisplayPictureBox_MouseDown(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseDown, DisplayPictureBox.MouseMove
+        Static oldX As Integer
+        Static oldY As Integer
+        Static penColor As Color
+
+        'Me.Text = $"({e.X},{e.Y}) Button: {e.Button.ToString} Color: {penColor.ToString}"
+        Me.Text = $"({e.X},{e.Y}) Button: {e.Button.ToString} Color: {Me.mainPen.Color().ToString}"
+
+
+
+
+        Select Case e.Button
+            Case e.Button.Left
+                MouseDraw(oldX, oldY, e.X, e.Y)
+            Case e.Button.Right
+
+            Case e.Button.Middle
+                ColorDialog.ShowDialog()
+                penColor = ColorDialog.Color
+                'Case Else
+                'MsgBox("Something went wrong")
+        End Select
+        oldX = e.X
+        oldY = e.Y
+    End Sub
+
+    Sub DrawWave()
+        Dim _start As Point
+        Dim _end As Point
+
+        Dim wave As Graphics = DisplayPictureBox.CreateGraphics
+        Dim degrees As Integer
+        Dim angle As Decimal
+
+        Dim yOffset As Single = CSng(DisplayPictureBox.Height / 2)
+        wave.TranslateTransform(0, yOffset)
+
+        _start.X = 0
+        _start.Y = 0
+
+        '_end.X = 350
+        '_end.Y = -90
+
+        Dim xMax As Single = 360
+        Dim yMax As Single = 100
+
+        Dim xScale As Single = CSng(DisplayPictureBox.Width / xMax)
+        Dim yScale As Single = CSng(((DisplayPictureBox.Height) / 2) / yMax)
+        wave.ScaleTransform(xScale, yScale)
+
+        DisplayPictureBox.Refresh()
+
+        mainPen.Color = Color.Red
+
+        'Vi = Vp * sin(360 * f * t + theta) + DC
+        For i = 0 To CInt(xMax)
+            _end.X = i
+            _end.Y = CInt(yMax * Sin((PI / 180) * _end.X)) * -1
+            wave.DrawLine(mainPen, _start, _end)
+            _start.X = _end.X
+            _start.Y = _end.Y
+        Next
+
+        mainPen.Color = Color.Green
+        'plot Cos
+        _start.X = 0
+        _start.Y = 0
+        For i = 0 To CInt(xMax)
+            _end.X = i
+            _end.Y = CInt(yMax * Cos((PI / 180) * _end.X)) * -1
+            wave.DrawLine(mainPen, _start, _end)
+            _start.X = _end.X
+            _start.Y = _end.Y
+        Next
+        mainPen.Color = Color.Blue
+        'plot Tan
+        _start.X = 0
+        _start.Y = 0
+        For i = 0 To CInt(xMax)
+            _end.X = i
+            Try
+                _end.Y = CInt(yMax * Tan((PI / 180) * _end.X)) * -1
+            Catch
+
+            End Try
+
+            wave.DrawLine(mainPen, _start, _end)
+
+            _start.X = _end.X
+            _start.Y = _end.Y
+        Next
+
+        wave.Dispose()
+
+        'Me.g.TranslateTransform(0, yOffset * -1)
+        'dyStatusLabel.Text = CStr(0)
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DrawWaveformsButton_Click(sender As Object, e As EventArgs) Handles DrawWaveformsButton.Click
+        DrawWave()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        Shake()
+        DisplayPictureBox.Refresh()
+    End Sub
+
+    Sub Shake()
+        Dim shakeAmount = 25
+
+        For i = 1 To 10
+            Me.Top += shakeAmount
+            Me.Left += shakeAmount
+            Sleep(50)
+            shakeAmount *= -1
+        Next
+
+    End Sub
+End Class
